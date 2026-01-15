9d8235b3bf2c9c36d2f107be385c0abbb9b08ede
santhosh1856 <santhoshkumar@botaiml.com>
2026-01-15 16:31:09 +0530

    updated packages

--- rag/index.py
+++ rag/index.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings   # ⬅️ add this
-from qdrant_client import QdrantClient
-from qdrant_client.models import VectorParams, Distance
-from langchain_community.vectorstores import Qdrant
+from langchain_qdrant import QdrantVectorStore
+from langchain_ollama import OllamaEmbeddings
 
 # === CONFIG ===
 COLLECTION = "nodejs_pdf"
@@ -24,19 +22,12 @@ chunks = RecursiveCharacterTextSplitter(
 # 3. Ollama Nomic embedder
 embedder = OllamaEmbeddings(model="nomic-embed-text")
 
-# 4. Qdrant connection + collection
-client = QdrantClient(url=QDRANT)
-client.recreate_collection(
-    collection_name=COLLECTION,
-    vectors_config=VectorParams(size=768, distance=Distance.COSINE)
-)
-
-# 5. Store in Qdrant
-Qdrant.from_documents(
-    chunks,
-    embedder,
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedder,
     url=QDRANT,
     collection_name=COLLECTION
 )
+
 
 print(f"Stored {len(chunks)} chunks in Qdrant 🚀")
